b24268a2d9801d4ad0ab3e1f30d4960bafc9e837
_tam <tamjid84@yahoo.com>
2018-06-12 16:38:31 +0600

    category 1

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from shophome.models import TopOffer
 
 
 def blog_list(request):
+    offer = get_object_or_404(TopOffer)
     posts = Post.objects.filter(status='published').order_by('-create')
     template = 'blog/blog_list.html'
     context = {
+        'offer': offer,
         'posts': posts
     }
     return render(request, template, context)
 
 
 def blog_detail(request, pid):
+    offer = get_object_or_404(TopOffer)
     post = get_object_or_404(Post, id=pid)
-    # post = Post.objects.filter(id=pid)
     template = 'blog/blog_detail.html'
     context = {
+        'offer': offer,
         'post': post
     }
     return render(request, template, context)
--- src/products/views.py
+++ src/products/views.py
@@ -1,25 +1,36 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Brand, ProductCategory, Colours, Product, CategoryGroup
+from shophome.models import TopOffer
 
 
 def products_list(request):
-    men_categories = ProductCategory.objects.all()
+    offer = get_object_or_404(TopOffer)
+    brands = Brand.objects.filter(status='published')
+    categories = ProductCategory.objects.all()
     colours = Colours.objects.all()
     products = Product.objects.filter(status='published')
     template = 'products/products_list.html'
     context = {
+        'offer': offer,
         'products': products,
-        'men_categories': men_categories,
+        'categories': categories,
         'colours': colours,
+        'brands': brands,
     }
     return render(request, template, context)
 
 
 def product_detail(request, pid):
+    offer = get_object_or_404(TopOffer)
+    brands = Brand.objects.filter(status='published')
     product = get_object_or_404(Product, id=pid)
+    colours = Colours.objects.all()
     template = 'products/product_detail.html'
     context = {
-        'product': product
+        'offer': offer,
+        'product': product,
+        'brands': brands,
+        'colours': colours,
     }
     return render(request, template, context)
 
