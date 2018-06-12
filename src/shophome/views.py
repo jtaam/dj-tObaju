cac4eb20610d1c85ef3ebffa53dacb3a352f7b42
_tam <tamjid84@yahoo.com>
2018-06-12 14:28:41 +0600

    from blog

--- src/shophome/views.py
+++ src/shophome/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from .models import TopSlider, Advantages, GetInspired, TopOffer
+from products.models import Product
+from blog.models import Post
 
 
 def homepage(request):
     offer = get_object_or_404(TopOffer)
     sliders = TopSlider.objects.filter(status='published')
     advantages = Advantages.objects.filter(status='published')
+    products = Product.objects.filter(status='published')
     get_inspired_images = GetInspired.objects.filter(status='published')
+    posts = Post.objects.filter(status='published')[0:2]
     template = 'homepage/homepage.html'
     context = {
         'offer': offer,
         'sliders': sliders,
         'advantages': advantages,
+        'products': products,
         'inspires': get_inspired_images,
-        'title': 'home page'
+        'posts': posts,
+        'title': 'home page',
     }
     return render(request, template, context)
