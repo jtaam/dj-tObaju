c354282c13c7a760fdbb4d992b7281da77a19c6e
_tam <tamjid84@yahoo.com>
2018-06-15 16:44:01 +0600

    newsletter admin form

---
+++ src/newsletter/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from crispy_forms.helper import FormHelper
+from .models import NewsletterUser, Newsletter
+
+
+class NewsletterUserSignUpForm(forms.ModelForm):
+    helper = FormHelper()
+    helper.form_show_labels = False
+
+    class Meta:
+        model = NewsletterUser
+        fields = ['email']
+
+        def clean_email(self):
+            email = self.cleaned_data.get('email')
+            return email
+
+
+class NewsletterCreationForm(forms.ModelForm):
+    class Meta:
+        model = Newsletter
+        fields = ['subject', 'body', 'email', 'status']
