fcc03120ff65ee4f7d66d541d35cd6565325bfee
_tam <tamjid84@yahoo.com>
2018-06-16 02:00:34 +0600

    messages front

--- src/newsletter/forms.py
+++ src/newsletter/forms.py
@@ -1,11 +1,21 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from .models import NewsletterUser, Newsletter
 
 
 class NewsletterUserSignUpForm(forms.ModelForm):
+    helper = FormHelper()
+    helper.form_show_labels = False
+    helper.add_input(Submit('submit', 'Subscribe'))
+
     class Meta:
         model = NewsletterUser
         fields = ['email']
+
+        def clean_email(self):
+            email = self.cleaned_data.get('email')
+            return email
 
 
 class NewsletterCreationForm(forms.ModelForm):
