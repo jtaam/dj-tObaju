351be8aa25771115268658cb1be1f146d84a5084
jtaam <tam@passion.oracledom.dev>
2019-04-18 22:30:40 +0600

    upgraded Django to 1.11.17 and Pillow to 6.0.0

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/tam/Developments/Python/Projects/Django/dj-tObaju/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
